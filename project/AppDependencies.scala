40453b900b4724f7e8df01fa362baa6c86ef2d2c
Andrew Smith <1895818+DevAndyroo@users.noreply.github.com>
2025-06-16 15:42:35 +0100

    Update play versiong (#22)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.12.0"
+  private val bootstrapVersion = "9.13.0"
   private val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "11.13.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.1.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                % "10.0.0",
