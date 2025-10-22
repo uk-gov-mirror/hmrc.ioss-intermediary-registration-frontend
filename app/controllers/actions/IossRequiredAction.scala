e3599a8ef26cfb18cd2feb64c9cd6de1fc7caf07
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-10-22 16:43:25 +0100

    create IossRequiredAction

---
+++ app/controllers/actions/IossRequiredAction.scala
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import logging.Logging
+import models.requests.{AuthenticatedDataRequest, AuthenticatedMandatoryIossRequest}
+import play.api.mvc.Results.*
+import play.api.mvc.{ActionRefiner, Result}
+import utils.FutureSyntax.FutureOps
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class IossRequiredActionImpl @Inject()()(implicit val executionContext: ExecutionContext)
+  extends ActionRefiner[AuthenticatedDataRequest, AuthenticatedMandatoryIossRequest] with Logging {
+
+  override protected def refine[A](request: AuthenticatedDataRequest[A]):
+  Future[Either[Result, AuthenticatedMandatoryIossRequest[A]]] = {
+    request.iossNumber match {
+      case None =>
+        println("REFINE DEF HIT")
+        logger.info("insufficient IossNumber enrolments")
+        Left(Unauthorized).toFuture
+
+      case Some(iossNumber) =>
+        request.registrationWrapper match {
+          case Some(registrationWrapper) =>
+            request.iossNumber match {
+              case None =>
+                logger.info("insufficient IOSS NUMBER enrolments")
+                Left(Unauthorized).toFuture
+
+              case Some(iossNumber) =>
+
+
+
+                Right(
+                  AuthenticatedMandatoryIossRequest(
+                    request = request,
+                    credentials = request.credentials,
+                    vrn = request.vrn,
+                    enrolments = request.enrolments,
+                    userAnswers = request.userAnswers,
+                    iossNumber = iossNumber,
+                    numberOfIossRegistrations = request.numberOfIossRegistrations,
+                    latestIossRegistration = request.latestIossRegistration,
+                    latestOssRegistration = request.latestOssRegistration,
+                    registrationWrapper = registrationWrapper
+                  )
+                ).toFuture
+              case _ =>
+                logger.error(s"Error: there was no registration present")
+                Left(InternalServerError).toFuture
+            }
+        }
+    }
+  }
+}
+
+class IossRequiredAction @Inject()()(implicit executionContext: ExecutionContext) {
+
+  def apply(): IossRequiredActionImpl = {
+    new IossRequiredActionImpl()
+  }
+}
