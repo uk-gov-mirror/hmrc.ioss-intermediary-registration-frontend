fa9696131dc898a135509c38acef35d5755ec033
Tayamul <100432502+Tayamul@users.noreply.github.com>
2025-10-27 10:41:36 +0000

    VEI-409: Implement `StartRejoinJourneyController`.

---
+++ app/controllers/rejoin/StartRejoinJourneyController.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.rejoin
+
+import connectors.RegistrationConnector
+import controllers.actions.*
+import logging.Logging
+import play.api.mvc.{Action, MessagesControllerComponents}
+import pages.{JourneyRecoveryPage, Waypoints}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.AnyContent
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import utils.FutureSyntax.FutureOps
+
+import java.time.{Clock, LocalDate}
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class StartRejoinJourneyController @Inject()(
+                                              override val messagesApi: MessagesApi,
+                                              cc: AuthenticatedControllerComponents,
+                                              registrationConnector: RegistrationConnector,
+                                              val controllerComponents: MessagesControllerComponents,
+                                              clock: Clock
+                                            )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport with Logging {
+
+  def onPageLoad(waypoints: Waypoints): Action[AnyContent] = cc.authAndRequireIntermediary(waypoints, inAmend = true, inRejoin = true).async {
+    implicit request =>
+
+      registrationConnector.displayRegistration(request.intermediaryNumber).flatMap {
+        case Right(registrationWrapper) =>
+          val currentDate: LocalDate = LocalDate.now(clock)
+          val canRejoin = registrationWrapper.etmpDisplayRegistration.canRejoinScheme(currentDate)
+
+          if(canRejoin) {
+            Redirect(controllers.rejoin.routes.RejoinSchemeController.onPageLoad().url).toFuture
+          } else {
+            Redirect(JourneyRecoveryPage.route(waypoints).url).toFuture
+          }
+
+        case Left(error) =>
+          val exception = new Exception(error.body)
+          logger.error(exception.getMessage, exception)
+          throw exception
+      }
+  }
+}
+
