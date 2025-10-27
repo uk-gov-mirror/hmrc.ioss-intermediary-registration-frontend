a833564f2668f7ddd10157d75a0e32afc2cd1fef
Tayamul <100432502+Tayamul@users.noreply.github.com>
2025-10-27 12:24:34 +0000

    VEI-409: Add unit tests for the .canRejoinScheme method.

--- test/models/etmp/display/EtmpDisplayRegistrationSpec.scala
+++ test/models/etmp/display/EtmpDisplayRegistrationSpec.scala
@@ -17,7 +17,13 @@
 package models.etmp.display
 
 import base.SpecBase
+import models.etmp.{EtmpExclusion, EtmpExclusionReason}
+import models.etmp.EtmpExclusionReason.*
+import org.scalatest.prop.TableDrivenPropertyChecks.forAll
+import org.scalatest.prop.Tables.Table
 import play.api.libs.json.{JsError, JsSuccess, Json}
+
+import java.time.LocalDate
 
 class EtmpDisplayRegistrationSpec extends SpecBase {
 
@@ -69,6 +75,82 @@ class EtmpDisplayRegistrationSpec extends SpecBase {
       )
 
       json.validate[EtmpDisplayRegistration] `mustBe` a[JsError]
+    }
+
+    ".canRejoinScheme" - {
+
+      val currentDate = LocalDate.now()
+
+      val nonReversalEtmpExclusionReasons = Table(
+        "etmpExclusionReason",
+        NoLongerSupplies,
+        CeasedTrade,
+        NoLongerMeetsConditions,
+        FailsToComply,
+        VoluntarilyLeaves,
+        TransferringMSID
+      )
+
+      def createExclusion(etmpExclusionReason: EtmpExclusionReason,
+                          effectiveDate: LocalDate = LocalDate.now(),
+                          quarantine: Boolean = false): EtmpExclusion = {
+        EtmpExclusion(
+          exclusionReason = etmpExclusionReason,
+          effectiveDate = effectiveDate,
+          decisionDate = LocalDate.now(),
+          quarantine = quarantine)
+      }
+
+      "return true" - {
+
+        "when it is not a reversal, not quarantined and the effective date is not in the future" in {
+          forAll(nonReversalEtmpExclusionReasons) { nonReversalEtmpExclusionReason =>
+            etmpDisplayRegistration.copy(exclusions = List(
+                createExclusion(nonReversalEtmpExclusionReason, effectiveDate = currentDate)
+              ))
+              .canRejoinScheme(currentDate) mustBe true
+          }
+        }
+
+        "when it is not a reversal, quarantined but the effectiveDate is more than 2 years ago" in {
+          forAll(nonReversalEtmpExclusionReasons) { nonReversalEtmpExclusionReason =>
+            etmpDisplayRegistration.copy(exclusions = List(
+                createExclusion(nonReversalEtmpExclusionReason, effectiveDate = currentDate.minusYears(2).minusDays(1), quarantine = true))
+              )
+              .canRejoinScheme(currentDate) mustBe true
+          }
+        }
+      }
+
+      "return false" - {
+
+        "for an empty exclusion list" in {
+          etmpDisplayRegistration.copy(exclusions = List.empty).canRejoinScheme(currentDate) mustBe false
+        }
+
+        "when the exclusion reason is a Reversal" in {
+          etmpDisplayRegistration.copy(exclusions = List(createExclusion(Reversal)))
+            .canRejoinScheme(currentDate) mustBe false
+        }
+
+        "when the exclusion reason is not a Reversal and it is quarantined with an effective date is above or equal to 2 years ago" in {
+
+          val dates = Table(
+            "date",
+            LocalDate.now().minusYears(2),
+            LocalDate.now().minusYears(2).plusDays(1),
+          )
+
+          forAll(nonReversalEtmpExclusionReasons) { etmpExclusionReason =>
+            forAll(dates) { date =>
+              etmpDisplayRegistration.copy(exclusions = List(
+                  createExclusion(etmpExclusionReason, effectiveDate = date, quarantine = true)
+                ))
+                .canRejoinScheme(currentDate) mustBe false
+            }
+          }
+        }
+      }
     }
   }
 }
