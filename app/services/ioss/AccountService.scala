e5771049343983694edbec80bb641f602e50d6ba
Antony100 <25503510+Antony100@users.noreply.github.com>
2025-10-28 14:23:24 +0000

    add getPreviousRegistrations to AccountService

--- app/services/ioss/AccountService.scala
+++ app/services/ioss/AccountService.scala
@@ -18,8 +18,10 @@ package services.ioss
 
 import config.Constants.iossEnrolmentKey
 import connectors.RegistrationConnector
+import models.amend.PreviousRegistration
 import uk.gov.hmrc.http.HeaderCarrier
 
+import java.time.LocalDate
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -35,6 +37,25 @@ class AccountService @Inject()(
         .identifiers
         .find(_.key == iossEnrolmentKey)
         .map(_.value)
+    }
+  }
+
+  def getPreviousRegistrations()(implicit hc: HeaderCarrier): Future[Seq[PreviousRegistration]] = {
+    registrationConnector.getAccounts().map { accounts =>
+
+      val accountDetails: Seq[(LocalDate, String)] = accounts
+        .enrolments.map(e => e.activationDate -> e.identifiers.find(_.key == "IOSSNumber").map(_.value))
+        .collect {
+          case (Some(activationDate), Some(intermediaryNumber)) => LocalDate.from(activationDate) -> intermediaryNumber
+        }.sortBy(_._1)
+
+      accountDetails.zip(accountDetails.drop(1)).map { case ((activationDate, intermediaryNumber), (nextActivationDate, _)) =>
+        PreviousRegistration(
+          startPeriod = activationDate,
+          endPeriod = nextActivationDate.minusMonths(1),
+          intermediaryNumber = intermediaryNumber
+        )
+      }
     }
   }
 }
